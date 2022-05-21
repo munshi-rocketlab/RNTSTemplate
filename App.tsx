4285af8096431c542430f250122bce47cdb5f9c1
Munshi <99158046+munshi-rocketlab@users.noreply.github.com>
2022-05-21 22:47:42 +1000

    Initial commit
    Signed-off-by: Munshi <99158046+munshi-rocketlab@users.noreply.github.com>

---
+++ App.tsx
@@ -0,0 +1,18 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import AppTemplate from 'components/AppTemplate';
+import React from 'react';
+
+const App = () => {
+  return <AppTemplate />;
+};
+
+export default App;
